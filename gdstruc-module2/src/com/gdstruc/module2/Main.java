b384af34406301018dea75ba1c7b7bb04063cead
chrissaliz <chrissaliz.batistil@benilde.edu.ph>
2021-08-31 18:24:29 +0800

    modified code for consultation, tuesday consult wrong code no node

--- gdstruc-module2/src/com/gdstruc/module2/Main.java
+++ gdstruc-module2/src/com/gdstruc/module2/Main.java
@@ -2,8 +2,10 @@ package com.gdstruc.module2;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+      {
 
+        player dracula = new player(666, "Dracula", 666);
         player asuna = new player(1, "Asuna", 100);
         player lethalBacon = new player(2, "LethalBacon", 205);
         player hpDeskjet = new player(3, "HpDeskJet", 34);
@@ -13,7 +15,22 @@ public class Main {
         playerLinkList.addToFront(asuna);
         playerLinkList.addToFront(lethalBacon);
         playerLinkList.addToFront(hpDeskjet);
+        playerLinkList.addToFront(dracula);
 
         playerLinkList.printList();
+
+        playerLinkList.removeNode();
+
+        System.out.print("#1 Remove First Element: ");
+        playerLinkList.printList();
+
+        System.out.print("#2 Count element:");
+        playerLinkList.countNode();
+
+        playerLinkList.containsNode(asuna);
+
+
+        }
     }
-}
+
+
--- gdstruc-module2/src/com/gdstruc/module2/playerLinkList.java
+++ gdstruc-module2/src/com/gdstruc/module2/playerLinkList.java
@@ -9,14 +9,50 @@ public class playerLinkList {
         head = playerNode;
     }
 
-    public void printList() {
+    public void removeNode (){
+
+        playerNode node = head;
+        head = head.getNextPlayer();
+
+    }
+
+    public void countNode () {
+        playerNode temp = head;
+        int count = 0;
+        while (temp != null)
+        {
+            count++;
+            temp = temp.getNextPlayer();
+        }
+
+        System.out.print(count);
+    }
+
+    public boolean containsNode (player player) {
+
+        player current = player;
+
+        while (current != null) {
+
+            if (current == player) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+        public void printList() {
+
         playerNode current = head;
         System.out.print("Head ->");
-        while (current!=null) {
-            System.out.print(current);
+        while (current != null) {
+            System.out.print(current.getPlayer());
             System.out.print(" -> ");
             current = current.getNextPlayer();
         }
         System.out.println("null");
     }
+
 }
